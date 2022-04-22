1ba1ca45d1e9564d4378a64c2749b56ad6c6bfc1
TWB1 <thewhildishbrothers@gmail.com>
2022-04-23 04:02:53 +0530

    Added Implicit and Explicit Conversion

---
+++ Basics/Implicit And Explicit Conversions/Program.cs
@@ -0,0 +1,38 @@
+﻿double myDouble = 13.37;
+int myInt;
+#region  Explicit
+//explicit conversion
+//cast double to integer
+myInt = (int)myDouble;
+Console.WriteLine(myInt);
+#endregion
+
+#region Implicit
+//implicit conversion
+int num = 12390532;
+long bigNum = num;
+
+float myFloat = 13.37f;
+double myDouble1 = myFloat;
+#endregion
+
+#region TypeConversion
+string myString = myDouble.ToString();
+string myString1 = num.ToString();
+
+
+bool sunIsShining = false;
+
+
+string sunIsShiningString = sunIsShining.ToString();
+Console.WriteLine(sunIsShiningString);
+#endregion
+
+
+#region Parse
+string myIntegerString1 = "13";
+string myIntegerString2 = "20";
+int myInteger = Int32.Parse(myIntegerString1);
+int myInteger2 = Int32.Parse(myIntegerString2);
+Console.WriteLine(myInteger + myInteger2);
+#endregion
