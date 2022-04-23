deec861002954ad9b6e7070d930b2a1a1530d138
TWB1 <thewhildishbrothers@gmail.com>
2022-04-23 21:11:49 +0530

    Added nested if

---
+++ Basics/Nested If/Program.cs
@@ -0,0 +1,17 @@
+﻿
+bool isAdmin = false, isRegistered = true;
+string userName = "";
+Console.WriteLine("Please Enter your username");
+userName = Console.ReadLine();
+if(isRegistered)
+{
+    Console.WriteLine("Hi there, registered user");
+    if(userName != "")
+    {
+        Console.WriteLine($"Hi there, {userName}");
+        if(userName.Equals("admin"))
+        {
+            Console.WriteLine("Hi there, admin");
+        }
+    }
+}
