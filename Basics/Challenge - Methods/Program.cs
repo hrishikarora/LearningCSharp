0abca6524e01f7c27d3e1005542136bb33d48705
TWB1 <thewhildishbrothers@gmail.com>
2022-04-23 18:28:00 +0530

    Added Challenge Method

---
+++ Basics/Challenge - Methods/Program.cs
@@ -0,0 +1,9 @@
+﻿string friendName1 = "Ishan" , friendName2 = "Hrishik" , friendName3 = "Friend 3";
+GreetFriend(friendName1,friendName2,friendName3);
+
+void GreetFriend(string friendName1 , string friendName2 , string friendName3)
+{
+    Console.WriteLine($"Hi {friendName1},my friend!");
+    Console.WriteLine($"Hi {friendName2},my friend!");
+    Console.WriteLine($"Hi {friendName3},my friend!");
+}
