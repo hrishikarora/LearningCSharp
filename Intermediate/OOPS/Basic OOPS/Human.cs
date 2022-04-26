279f95909e42548fc041420bece18ba8bb74ab4a
TWB1 <thewhildishbrothers@gmail.com>
2022-04-26 18:53:28 +0530

    Added OOPS

---
+++ Intermediate/OOPS/Basic OOPS/Human.cs
@@ -0,0 +1,15 @@
+namespace OOPS
+{
+    public class Human
+    {
+        // variable first name and full name
+        public string firstName = "Random";
+        public string lastName = "Guy";
+
+        public void IntroduceYourself()
+        {
+            Console.WriteLine($"Hi, I am {firstName} {lastName}");
+        }
+
+    }
+}
---
+++ Intermediate/OOPS/Basic OOPS/basicOOPS.cs
@@ -0,0 +1,10 @@
+﻿using OOPS;
+Human hrishik = new Human();
+hrishik.firstName = "Hrishik";
+hrishik.lastName = "Arora";
+hrishik.IntroduceYourself();
+
+Human random = new Human();
+random.firstName = "Random";
+random.lastName = "Guy";
+random.IntroduceYourself();
