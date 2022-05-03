762b75f8e9d280db90ed47d28ae01b2572bc7a5e
TWB1 <thewhildishbrothers@gmail.com>
2022-05-03 15:12:42 +0530

    Added arrays initialization

---
+++ Intermediate/Collections/Arrays/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] arrayFirst = new int[5];
+arrayFirst[0] = 11;
+arrayFirst[1] = 2;
+arrayFirst[2] = 3;
+arrayFirst[3] = 4;
+arrayFirst[4] = 5;
+
+Console.WriteLine("Array first elemet" + " " + arrayFirst[0]);
+
+int[] arraySecond = { 20, 10, 11, 4, 11 };
+
+Console.WriteLine($"ArraySecond first element {arraySecond[0]}");
+
+int[] arrayThird = new int[] { 1, 2, 3, 4, 5 };
+
+Console.WriteLine($"ArrayThird first element {arrayThird[0]}");
+
